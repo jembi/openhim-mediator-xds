f010b90c37e7583830653f1ca937a57a19fb85be
Ryan Crichton <rg.crichton@gmail.com>
2015-01-22 11:05:19 +0200

    Changed mock pix server to use an ID that is allocated in the IHE NIST tools

--- src/test/resources/mock-services/mock-pix-server.js
+++ src/test/resources/mock-services/mock-pix-server.js
@@ -10,7 +10,7 @@ var footer = String.fromCharCode(034) + '\r';
 var cannedResponse = 'MSH|^~\\&|PIX_MGR_OpenHIE_CR_MCAAT|OPENHIE|openhim|openhim-mediator-ohie-xds|201411210846||RSP^K23^RSP_K23|655ba7d4-71c0-450d-a5e1-69e97d68f944|P|2.5\r\n'
   + 'MSA|AA|84efdb97-d0f4-42bf-bdb5-1752eba0592b\r\n'
   + 'QAK|be7578db-4619-4cad-a320-38bad0b7cccf|OK\r\n'
-  + 'PID|||f10f8d972aba4fd^^^&1.3.6.1.4.1.21367.2005.13.20.3000&ISO^PI||~^^^^^^S'
+  + 'PID|||fc133984036647e^^^&1.3.6.1.4.1.21367.2005.13.20.3000&ISO^PI||~^^^^^^S'
 
 net.createServer(function(c) {
 
